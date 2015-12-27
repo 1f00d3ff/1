ee83f02ac36338575eabe6861805fc02d5d96b7f
1f00d3ff <1f00d3ff@opayq.com>
2015-12-27 16:05:47 +0000

    dm setup

---
+++ awesomeDM.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+declare -a dependencies=("awesome" "xclip" "parcellite" "rxvt-unicode-256color" "terminus")
+
+for i in "${dependencies[@]}"
+do
+  if ! dpkg -l $i > /dev/null
+  then
+  sudo apt-get install $i -y
+  fi
+done;
+
+rm /usr/share/awesome/themes/default/theme.lua
+sudo ln -s ~/0/theme.lua /usr/share/awesome/themes/default/theme.lua
+
+mkdir -p ~/.config/awesome
+sudo ln -s ~/0/rc.lua ~/.config/awesome/rc.lua
