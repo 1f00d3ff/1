d7dc6c824fd2486c7251d234f8d6b42e1382871f
1f00d3ff <1f00d3ff@opayq.com>
2016-01-12 14:49:26 -0600

    kpcli & espeak added to dependencies

--- dependencies.sh
+++ dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-declare -a dependencies=("build-essential" "dnsutils" "gnupg" "htop" "irssi" "jq" "knockd" "ntp" "oathtool" "openssl" "openvpn" "rhash" "screen" "tesseract-ocr" "tesseract-ocr-eng" "tmux" "vim" "w3m" "wget" "whois" "wikipedia2text")
+declare -a dependencies=("build-essential" "dnsutils" "espeak" "gnupg" "htop" "irssi" "jq" "knockd" "kpcli" "libterm-readline-gnu-perl" "ntp" "oathtool" "openssl" "openvpn" "rhash" "screen" "tesseract-ocr" "tesseract-ocr-eng" "tmux" "vim" "w3m" "wget" "whois" "wikipedia2text")
 
 for i in "${dependencies[@]}"
 do
