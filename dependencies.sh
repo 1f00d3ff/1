5bd37634ede4ce82511ff66a40fccfa8db2f5525
1f00d3ff <1f00d3ff@opayq.com>
2016-01-16 23:05:44 -0600

    update

--- dependencies.sh
+++ dependencies.sh
@@ -2,6 +2,8 @@
 
 declare -a dependencies=("build-essential" "dnsutils" "espeak" "gnupg" "htop" "irssi" "jq" "knockd" "kpcli" "libterm-readline-gnu-perl" "ntp" "oathtool" "openssl" "openvpn" "rhash" "screen" "tesseract-ocr" "tesseract-ocr-eng" "tmux" "vim" "w3m" "wget" "whois" "wikipedia2text")
 
+sudo apt-get update
+
 for i in "${dependencies[@]}"
 do
   if ! dpkg -l $i > /dev/null
