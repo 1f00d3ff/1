efa35d5617f2bdbd555b55c94ae081bedc783f6c
1f00d3ff <1f00d3ff@opayq.com>
2016-01-19 16:00:11 -0600

    removed #

--- irssi.sh
+++ irssi.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-#CPUS=$(($(cat /proc/cpuinfo | grep processor | wc -l)+1))
-#
-#sudo apt-get install irssi-dev
-#sudo apt-get install libloudmouth1-dev 
-#cd ~ && wget -O - "https://github.com/cdidier/irssi-xmpp/archive/v0.52.tar.gz" | tar zxf -
-#cd ~/irssi-xmpp-0.52
-#
-#export IRSSI_INCLUDE=/usr/include/irssi/
-#
-#make -j $CPUS
-#
-#sudo make PREFIX=/usr install
-#
+CPUS=$(($(cat /proc/cpuinfo | grep processor | wc -l)+1))
+
+sudo apt-get install irssi-dev
+sudo apt-get install libloudmouth1-dev 
+cd ~ && wget -O - "https://github.com/cdidier/irssi-xmpp/archive/v0.52.tar.gz" | tar zxf -
+cd ~/irssi-xmpp-0.52
+
+export IRSSI_INCLUDE=/usr/include/irssi/
+
+make -j $CPUS
+
+sudo make PREFIX=/usr install
+
 mkdir -p ~/.irssi/scripts/autorun/
 
 wget -O - "https://scripts.irssi.org/scripts/nickcolor.pl" > ~/.irssi/scripts/nickcolor.pl
